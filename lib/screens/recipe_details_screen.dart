cd5f756e3bf9f8cb4b7d206ea25e6339916e34b7
bl1nkker <nurovich14@gmail.com>
2022-05-16 19:10:22 +0600

    feat: restyled recipe details screen

--- lib/screens/recipe_details_screen.dart
+++ lib/screens/recipe_details_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rosemarin_recipe_app/color_styles.dart';
+import 'package:rosemarin_recipe_app/models/ingredient_model.dart';
+import 'package:rosemarin_recipe_app/models/product_model.dart';
 import 'package:rosemarin_recipe_app/models/recipe_model.dart';
+import 'package:rosemarin_recipe_app/state/recipes_manager.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:collection/collection.dart';
 
 class RecipeDetailsScreen extends StatefulWidget {
   final RecipeModel recipe;
@@ -31,15 +36,16 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
             parallaxOffset: .1,
 
             panelBuilder: (controller) => DetailsPanelWidget(
-                controller: controller, panelController: panelController),
+                recipe: widget.recipe,
+                controller: controller,
+                panelController: panelController),
             body: Container(
-                // TODO: Change content padding value to constant
-
                 height: 250,
                 decoration: BoxDecoration(
                   // TODO: Set default image color
                   color: ColorStyles.accentColor,
                   image: DecorationImage(
+                      alignment: Alignment.topCenter,
                       image: NetworkImage(
                         widget.recipe.image,
                       ),
@@ -56,7 +62,11 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SizedBox(
+                          Container(
+                            decoration: BoxDecoration(
+                              color: ColorStyles.accentColor,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
                             width: 50,
                             height: 50,
                             child: GestureDetector(
@@ -65,7 +75,7 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                               },
                               child: const Icon(Icons.arrow_back_ios_sharp,
                                   // TODO: Work with colors
-                                  color: ColorStyles.primaryColor),
+                                  color: ColorStyles.secondaryColor),
                             ),
                           ),
                         ],
@@ -80,19 +90,101 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
   }
 }
 
-class DetailsPanelWidget extends StatelessWidget {
+class DetailsPanelWidget extends StatefulWidget {
+  final RecipeModel recipe;
   final ScrollController controller;
   final PanelController panelController;
   const DetailsPanelWidget(
-      {Key? key, required this.controller, required this.panelController})
+      {Key? key,
+      required this.recipe,
+      required this.controller,
+      required this.panelController})
       : super(key: key);
 
+  @override
+  State<DetailsPanelWidget> createState() => _DetailsPanelWidgetState();
+}
+
+class _DetailsPanelWidgetState extends State<DetailsPanelWidget> {
+  int totalFats = 0;
+  int totalCarbs = 0;
+  int totalProteins = 0;
+  int totalCalories = 0;
   void togglePanel() {
-    if (panelController.isPanelOpen) {
-      panelController.close();
+    if (widget.panelController.isPanelOpen) {
+      widget.panelController.close();
     } else {
-      panelController.open();
+      widget.panelController.open();
     }
+  }
+
+  List<Widget> createTagChips(BuildContext context, List<int> ingredientIds) {
+    final List<IngredientModel> ingrediens = ingredientIds
+        .map((id) => Provider.of<RecipesManager>(context, listen: false)
+            .ingredients
+            .firstWhere((ingredient) => ingredient.id == id))
+        .toList();
+    final List<ProductModel?> usedProducts = ingrediens
+        .map((ingredient) => Provider.of<RecipesManager>(context, listen: false)
+            .products
+            .firstWhereOrNull(
+                (product) => ingredient.name.contains(product.title)))
+        .toList();
+    setState(() {
+      totalFats =
+          usedProducts.fold(0, (sum, product) => sum + (product?.fats ?? 0));
+      totalCarbs =
+          usedProducts.fold(0, (sum, product) => sum + (product?.carbs ?? 0));
+      totalProteins = usedProducts.fold(
+          0, (sum, product) => sum + (product?.proteins ?? 0));
+      totalCalories = usedProducts.fold(
+          0, (sum, product) => sum + (product?.calories ?? 0));
+    });
+
+    final chips = <Widget>[];
+    for (var element in ingrediens) {
+      final chip = Row(children: [
+        Chip(
+            label: Text(
+              element.name[0].toUpperCase() + element.name.substring(1),
+              style: const TextStyle(
+                  color: ColorStyles.secondaryColor,
+                  fontWeight: FontWeight.w300),
+            ),
+            backgroundColor: ColorStyles.accentColor),
+        const SizedBox(width: 5),
+        Text(
+          " x ${element.quantity.toString()} ${element.unit}",
+          style: const TextStyle(
+              color: ColorStyles.secondaryColor, fontWeight: FontWeight.w300),
+        ),
+      ]);
+      chips.add(chip);
+    }
+
+    return chips;
+  }
+
+  Widget buildIngredientInfoRow(
+      {required IconData icon, required String title, required String data}) {
+    return Row(
+      children: [
+        Row(
+          children: [
+            Icon(icon, color: ColorStyles.accentColor),
+            Text(title,
+                style: const TextStyle(
+                    color: ColorStyles.secondaryColor,
+                    fontWeight: FontWeight.w300)),
+          ],
+        ),
+        const SizedBox(width: 10),
+        Text(data,
+            style: const TextStyle(
+                color: ColorStyles.secondaryColor,
+                fontWeight: FontWeight.w300)),
+      ],
+    );
   }
 
   @override
@@ -127,28 +219,62 @@ class DetailsPanelWidget extends StatelessWidget {
               const SizedBox(height: 10.0),
               Expanded(
                 child: ListView(
-                  controller: controller,
+                  controller: widget.controller,
                   children: [
-                    Text(
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                        style: themeData.textTheme.bodyText2),
+                    Text(widget.recipe.title,
+                        style: const TextStyle(
+                            color: ColorStyles.secondaryColor,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w400)),
                     const SizedBox(
                       height: 20.0,
+                    ),
+                    const Text('Ingredients: ',
+                        style: TextStyle(
+                            color: ColorStyles.secondaryColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400)),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ...createTagChips(
+                            context, widget.recipe.ingredients_ids),
+                      ],
                     ),
                     const SizedBox(
                       height: 20.0,
                     ),
-                    Text(
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                        style: themeData.textTheme.bodyText2),
+                    const Text('Nutrients: ',
+                        style: TextStyle(
+                            color: ColorStyles.secondaryColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400)),
                     const SizedBox(
                       height: 20.0,
                     ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    Text('Здесь будут ссылки. Пока не знаю в каком формате',
-                        style: themeData.textTheme.bodyText2)
+                    Column(
+                      children: [
+                        buildIngredientInfoRow(
+                            icon: Icons.foggy,
+                            title: 'Total fats:',
+                            data: totalFats.toString()),
+                        buildIngredientInfoRow(
+                            icon: Icons.fastfood,
+                            title: 'Total carbs:',
+                            data: totalCarbs.toString()),
+                        buildIngredientInfoRow(
+                            icon: Icons.fastfood,
+                            title: 'Total proteins:',
+                            data: totalProteins.toString()),
+                        buildIngredientInfoRow(
+                            icon: Icons.fastfood,
+                            title: 'Total calories:',
+                            data: totalCalories.toString()),
+                      ],
+                    )
                   ],
                 ),
               ),
